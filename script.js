e5aa870dab654768563309318e2d0db5d76208cf
bivaly <71719024+Kisbivaly@users.noreply.github.com>
2024-09-10 20:18:44 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Később ezt ki lehet egészíteni input mezőkkel és file feltöltéssel
+document.getElementById('name').textContent = "Minta Anita Dóra";
+document.getElementById('profession').textContent = "Turisztikai asszisztens";
+document.getElementById('about-text').textContent = "Nagy felelősségtudattal rendelkező, megbízható, körültekintő személy vagyok...";
